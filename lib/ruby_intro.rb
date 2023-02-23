0d7e5acedffb1f4724b27244552abed1cfa0a8c9
markbowen-uccs <mbowen@uccs.edu>
2023-02-23 02:43:49 +0000

    Ruby_Intro 2_22_2023

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,29 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.length > 1 && arr.permutation(2).any? { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  else
+    !['a', 'e', 'i', 'o', 'u', 'y'].include?(s[0][0].downcase) && !s.empty?
+  end
 end
 
 def binary_multiple_of_4? s
@@ -31,5 +35,28 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    unless isbn != ''
+      raise ArgumentError
+    end
+    unless price > 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn
+    @isbn
+  end
+
+  def price
+    @price
+  end
+
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
+
 end
